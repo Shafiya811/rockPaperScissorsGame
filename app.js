921f23e026402374987a33bb92ca220c0d7d8e2c
Shafiya811 <103237450+Shafiya811@users.noreply.github.com>
2024-01-23 20:30:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-alert("Select One choice");
 let userScore=0;
 let compScore=0;
 let choices= document.querySelectorAll(".choice");
@@ -14,24 +13,19 @@ const genCompChoice= ()=> {
 };
 
 const playGame = (userChoice) => {
-    console.log("user choice =", userChoice);
-    //computer choice generate
+    //computer choice generated
     const compChoice= genCompChoice();
-    console.log("Computer choice =", compChoice);
     if(userChoice == compChoice){
-        // console.log("Draw");
         msg.innerText= "It's a Draw. Try again";
         msg.style.backgroundColor = "#200E3A";
 
     }
     else if((userChoice=="paper"&& compChoice=="rock")||(userChoice=="rock"&& compChoice=="scissors")||(userChoice=="scissors"&& compChoice=="paper")){
-        // console.log("User won");
         msg.innerText= `You Won. ${userChoice} beats ${compChoice}.`;
         msg.style.backgroundColor = "green";
         userScore++;
     }
     else {
-        // console.log("Computer won");
         msg.innerText= `You lost. ${compChoice} beats ${userChoice}.`;
         msg.style.backgroundColor = "red";
         compScore++;
